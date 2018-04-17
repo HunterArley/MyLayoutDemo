beefa721c3488ff95751efa03d8d9b3858881ebd
hunterarley <18200399036@163.com>
2018-04-17 22:55:17 +0800

    示例1：注册页面

--- app/src/main/java/com/example/hunterarley/mylayoutdemo/MainActivity.java
+++ app/src/main/java/com/example/hunterarley/mylayoutdemo/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.hunterarley.mylayoutdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findView();
+    }
+
+    private void findView() {
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn1.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn1:
+                Intent intent = new Intent(this, Test1Activity.class);
+                startActivity(intent);
+                break;
+        }
+
     }
 }
