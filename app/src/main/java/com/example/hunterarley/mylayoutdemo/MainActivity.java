6624aef5e5e73b2441dac44554667298a0608336
hunterarley <18200399036@163.com>
2018-04-18 22:16:09 +0800

    示例3：登陆界面

--- app/src/main/java/com/example/hunterarley/mylayoutdemo/MainActivity.java
+++ app/src/main/java/com/example/hunterarley/mylayoutdemo/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private Button btn1,btn2;
+    private Button btn1,btn2,btn3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +21,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btn1.setOnClickListener(this);
         btn2 = (Button) findViewById(R.id.btn2);
         btn2.setOnClickListener(this);
+        btn3 = (Button) findViewById(R.id.btn3);
+        btn3.setOnClickListener(this);
     }
 
 
@@ -34,6 +36,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btn2:
                 Intent intent2 = new Intent(this, Test2Activity.class);
                 startActivity(intent2);
+                break;
+            case R.id.btn3:
+                Intent intent3 = new Intent(this, Test3Activity.class);
+                startActivity(intent3);
                 break;
         }
 
